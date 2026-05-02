fff11b4fa29315fbfbb401029ddb0c4435233231
roccabr <brunoldrocca@gmail.com>
2026-05-02 17:53:35 -0300

    Delete app directory

--- app/schemas.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, HttpUrl
-from typing import Optional
-
-
-class ExtractPortfolioRequest(BaseModel):
-    report_id: Optional[str] = None
-    portfolio_pdf_url: HttpUrl
